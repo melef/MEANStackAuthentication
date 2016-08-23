5b7844ab351ee9a873eaa6c7efb179399bf67bbd
ollyblink <oliver_zihler@hotmail.com>
2016-08-23 14:50:48 +0200

    deleted typings*

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/express/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node-forge/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/chai/index.d.ts" />
